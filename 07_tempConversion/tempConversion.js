e7f1b2706337443a434e1fa5510a7c6cdba3090c
Do Nam Binh <131113311+Do-Nam-Binh@users.noreply.github.com>
2023-06-29 17:53:32 +0700

    Complete exercises 6-8

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  return Number((Math.round((((temperature - 32) * 5) / 9) * 10) / 10).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return Number((Math.round(((temperature * 9/5) + 32) * 10) / 10).toFixed(1));
+  
 };
 
 // Do not edit below this line
